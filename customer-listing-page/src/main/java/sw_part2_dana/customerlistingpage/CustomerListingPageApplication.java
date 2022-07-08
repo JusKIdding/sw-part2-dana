82d7d757cd58569a3721418d0d2496fb9b256bf4
Dana <danaplng@gmail.com>
2022-07-09 00:14:33 +0800

    Revert back to step "Hello, World" due to misfunction

--- customer-listing-page/src/main/java/sw_part2_dana/customerlistingpage/CustomerListingPageApplication.java
+++ customer-listing-page/src/main/java/sw_part2_dana/customerlistingpage/CustomerListingPageApplication.java
@@ -1,12 +1,31 @@
 package sw_part2_dana.customerlistingpage;
 
+import java.util.*;
+import java.time.LocalDate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sw_part2_dana.Customer.Customer;
 
 @SpringBootApplication
+@RestController
 public class CustomerListingPageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerListingPageApplication.class, args);
 	}
+
+	@GetMapping
+	public List<String> hello(){
+		List<String> helloList = new ArrayList<String>();
+
+		helloList.add("Hello");
+		helloList.add("World");
+
+		return helloList;
+	}
+	
 }
