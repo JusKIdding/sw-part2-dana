5912bf720af6556ebaf882ebd6111416f34e1594
Dana <danaplng@gmail.com>
2022-07-08 11:04:08 +0800

    First commit

---
+++ customer-listing-page/src/main/java/sw_part2_dana/customerlistingpage/CustomerListingPageApplication.java
@@ -0,0 +1,13 @@
+package sw_part2_dana.customerlistingpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerListingPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerListingPageApplication.class, args);
+	}
+
+}
---
+++ customer-listing-page/src/test/java/sw_part2_dana/customerlistingpage/CustomerListingPageApplicationTests.java
@@ -0,0 +1,13 @@
+package sw_part2_dana.customerlistingpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerListingPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
