fe19cac53f83f14a6b546c161f741107e929cc2d
Dana <danaplng@gmail.com>
2022-07-10 03:13:20 +0800

    tried validing DOB, failed. Skip this validation and proceed with spring boot connection

--- Customer_FE/src/app/registration-customer-page/registration-customer-page.component.ts
+++ Customer_FE/src/app/registration-customer-page/registration-customer-page.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { Customer } from '../Customer/customer';
 import { CustomerAPIServiceService } from '../Customer/customer-apiservice.service';
 import { AgeValidator } from '../Customer/custom-validators/age.validator';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-registration-customer-page',
@@ -18,21 +19,30 @@ export class RegistrationCustomerPageComponent {
 
   customerForm = this.formBuilder.group({
     name: ['', [Validators.required, Validators.pattern("[a-zA-Z][a-zA-Z]+")]],
-    dob: '',
+    dob: ['', AgeValidator],
     idNo: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
-    regDate: ['', AgeValidator],
+    regDate: '',
     address: ''
   });
 
+  // age18Check(dob : Date){
+  //   return moment(dob).add(18, 'years') <= moment();
+  // }
+
   get age(){
-    return this.customerForm.get('regDate');
+    return this.customerForm.get('dob');
   }
 
   onSubmit(){
     if(!this.customerForm.valid){
       alert('Unable to create account. The customer is not of age of 18 and above.');
+      console.log(this.customerForm);
       return false;
-    } 
+    } else {
+      alert('Success');
+      console.log("Success::" + this.customerForm);
+      return true;
+    }
   }
 
   /*
