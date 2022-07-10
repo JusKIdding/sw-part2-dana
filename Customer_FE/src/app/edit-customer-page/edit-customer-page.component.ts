d201172c65068a51c74b7626ea98148d85bc78cf
Dana <danaplng@gmail.com>
2022-07-10 15:56:20 +0800

    1) Disable fields except for idNo and Address as per assessment

--- Customer_FE/src/app/edit-customer-page/edit-customer-page.component.ts
+++ Customer_FE/src/app/edit-customer-page/edit-customer-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { CustomerAPIServiceService } from '../Customer/customer-apiservice.service';
+
 
 @Component({
   selector: 'app-edit-customer-page',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCustomerPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private customerService: CustomerAPIServiceService
+  ) { }
+
+  customerForm = this.formBuilder.group({
+    name: {value: 'placeholder', disabled: true},
+    // name: ['', [Validators.required, Validators.pattern("[a-zA-Z][a-zA-Z]+")]],
+    dob: {value: '', disabled:true},
+    idNo: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+    regDate: {value: '', disabled:true},
+    address: ''
+  });
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log("Testing");
   }
 
 }
